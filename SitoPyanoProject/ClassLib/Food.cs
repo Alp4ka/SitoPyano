7144280ebdd18e2ba1ccb2f848a0d18b6eb5bc00
DilanBulamu <dbulamu@mail.ru>
2020-11-14 23:30:50 +0300

    Food

--- SitoPyanoProject/ClassLib/Food.cs
+++ SitoPyanoProject/ClassLib/Food.cs
@@ -6,5 +6,26 @@ namespace ClassLib
 {
     class Food
     {
+        public int mass;
+        public int Mass
+        {
+            get => mass;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Некореектный ввод.");
+                }
+                mass = value;
+            }
+        }
+        public Food(int mass,double price, int count,string name) : base(price, count, name)
+        {
+            Mass = mass;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Масса содержимог: {Mass}кг.";
+        }
     }
 }
