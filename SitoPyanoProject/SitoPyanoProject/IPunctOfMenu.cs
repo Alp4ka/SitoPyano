1535bc6a884d9aa5c6bc0e3ddd85c565e8d6139f
Roman <kisame2002@mail.ru>
2020-11-15 00:45:15 +0300

    asdda
    dasda

--- SitoPyanoProject/SitoPyanoProject/IPunctOfMenu.cs
+++ SitoPyanoProject/SitoPyanoProject/IPunctOfMenu.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace SitoPyanoProject
 {
     public interface IPunctOfMenu
     {
         public double Price { get; protected set; }
+        public string Name { get; protected set; }
+        public List<Product> Components { get; protected set; }
     }
 }
