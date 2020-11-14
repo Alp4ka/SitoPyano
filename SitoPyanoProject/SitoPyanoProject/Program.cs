ffb9496637ac3f1e575dc0fc2b463b564e549fe5
Roman <kisame2002@mail.ru>
2020-11-14 18:39:34 +0300

    Perviy Commit
    awdawdawdaw

---
+++ SitoPyanoProject/SitoPyanoProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SitoPyanoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
