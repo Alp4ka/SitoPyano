f4578572857a6fea1d29348de3e7465cdfda7622
Roman <kisame2002@mail.ru>
2020-11-14 23:35:09 +0300

    Mat' ebal

---
+++ SitoPyanoProject/SitoPyanoProject/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SitoPyanoProject
+{
+    class Food : Product
+    {
+        private int mass;
+        public int Mass
+        {
+            get => mass;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Некорректный ввод.");
+                }
+                mass = value;
+            }
+        }
+        public Food(int mass, double price, int count, string name) : base(price, count, name)
+        {
+            Mass = mass;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Масса содержимог: {Mass}кг.";
+        }
+    }
+}
