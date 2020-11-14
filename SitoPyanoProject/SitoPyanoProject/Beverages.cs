fa4256d50fb98e2f37b813881ad642d3fda67a0b
Roman <kisame2002@mail.ru>
2020-11-14 23:29:32 +0300

    dsasdasdfsdfsdfsd

---
+++ SitoPyanoProject/SitoPyanoProject/Beverages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SitoPyanoProject
+{
+    public class Beverages : Product
+    {
+        public double Size { get; }
+        public string BeverageType { get; }
+
+        public Beverages(string type, double size, string name, int count, double price) : base(price, count, name)
+        {
+            BeverageType = type;
+            Size = size;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{BeverageType}; {Size:F1}";
+        }
+    }
+}
---
+++ SitoPyanoProject/SitoPyanoProject/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace SitoPyanoProject
+{
+    public class Product
+    {
+        private double price;
+        private int count;
+        public double Price { 
+            get => price;
+            set
+            {
+                if(value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Неверное значение для цены продукта: {value}");
+                }
+            }
+        }
+        public string Name { get; private set; }
+        public int Count {
+            get => count;
+            set
+            {
+                if (value >= 0)
+                {
+                    count = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Неверное значение для количества продукта: {value}");
+                }
+            }
+        }
+        public Product(double price, int count, string name)
+        {
+            Price = price;
+            Count = count;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"Название : {Name}\n" +
+                $"Цена: {Price} руб.шт.\n" +
+                $"Количество: {Count} шт.\n";
+        }
+    }
+}
