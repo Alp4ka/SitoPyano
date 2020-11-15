59aa070d08c4362cb5ef1c8194dbec28dbed2753
DilanBulamu <dbulamu@mail.ru>
2020-11-15 14:53:45 +0300

    dgdg

---
+++ SitoPyanoProject/SitoPyanoProject/NameOfProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SitoPyanoProject
+{
+    class NameOfProduct
+    {
+        public static List<Product> allProducts = new List<Product>();
+        public static List<Product> AllProducts => allProducts;
+        public NameOfProduct(Product name)
+        {
+            allProducts.Add(name);
+        }
+    }
+}
